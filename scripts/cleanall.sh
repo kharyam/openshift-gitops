fb890f9a61c9cbd8f86afb706ffdc44b606313f5
Khary Mendez <kharyam@gmail.com>
2022-08-19 17:20:22 -0400

    typo

--- scripts/cleanall.sh
+++ scripts/cleanall.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 oc delete appset openshift-operators-instances --cascade=foreground --wait -n openshift-gitops
 oc delete appset openshift-operators --cascade=foreground --wait -n openshift-gitops
 oc get project --no-headers -o custom-columns=NAME:..metadata.name | grep knative | xargs oc delete project
-oc get csv -n openshift-operators --no-headers -o custom-columns=NAME:..metadata.name | grep -v gitops | xargs oc delete csv -n openshift-operators --cascade=foreground --wait
+oc get csv -n openshift-operators --no-headers -o custom-columns=NAME:.metadata.name | grep -v gitops | xargs oc delete csv -n openshift-operators --cascade=foreground --wait
 
 $SCRIPT_DIR/servicemeshcleanup.sh
