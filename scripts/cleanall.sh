c74a78feb9d89bd50627d6321a07d6a44ef74a0e
Khary Mendez <kharyam@gmail.com>
2022-10-17 21:55:22 -0400

    Fix cleanup script

--- scripts/cleanall.sh
+++ scripts/cleanall.sh
@@ -8,7 +8,8 @@ oc delete appset helm-apps --cascade=foreground --wait -n openshift-gitops
 oc get project --no-headers -o custom-columns=NAME:..metadata.name | grep knative | xargs oc delete project
 oc get csv -n openshift-operators --no-headers -o custom-columns=NAME:.metadata.name | grep -v gitops | xargs oc delete csv -n openshift-operators --cascade=foreground --wait
 oc get csv -n openshift-vertical-pod-autoscaler --no-headers -o custom-columns=NAME:.metadata.name | grep -v gitops | xargs oc delete csv -n openshift-operators --cascade=foreground --wait
+
 # Clean the catalog source cache
-oc -n openshift-operator-lifecycle-manager delete $(oc get pods  -l app=catalog-operator -o NAME -n openshift-operator-lifecycle-manager
+oc -n openshift-operator-lifecycle-manager delete $(oc get pods  -l app=catalog-operator -o NAME -n openshift-operator-lifecycle-manager)
 
 $SCRIPT_DIR/servicemeshcleanup.sh
